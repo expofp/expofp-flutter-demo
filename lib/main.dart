8cc59bb66eed0b2bc86561f80005071cbb2fe8ff
vladimir-nikitin <60826376+vladimir-nikitin@users.noreply.github.com>
2023-11-13 19:23:02 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Column(children: <Widget>[
+        Expanded(
+          child: InAppWebView(
+            initialUrlRequest:
+                URLRequest(url: Uri.parse('https://demo.expofp.com')),
+            initialOptions: InAppWebViewGroupOptions(
+                android: AndroidInAppWebViewOptions(
+                    allowFileAccess: true,
+                    domStorageEnabled: true,
+                    allowContentAccess: true),
+                crossPlatform: InAppWebViewOptions(
+                    allowUniversalAccessFromFileURLs: true,
+                    javaScriptEnabled: true,
+                    allowFileAccessFromFileURLs: true)),
+            onConsoleMessage: (InAppWebViewController controller,
+                ConsoleMessage consoleMessage) {},
+            onWebViewCreated: (InAppWebViewController controller) {
+              controller.addJavaScriptHandler(
+                  handlerName: 'onBoothClick',
+                  callback: (args) {
+                    //Some code
+                  });
+
+              controller.addJavaScriptHandler(
+                  handlerName: 'onFpConfigured',
+                  callback: (args) {
+                    //Some code
+                  });
+
+              controller.addJavaScriptHandler(
+                  handlerName: 'onDirection',
+                  callback: (args) {
+                    //Some code
+                  });
+            },
+          ),
+        )
+      ]),
+    );
+  }
+}
